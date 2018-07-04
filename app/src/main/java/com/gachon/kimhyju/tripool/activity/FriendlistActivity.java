d9543a4afd75f69b760006b6a9da5cb85b8c8256
kimhyju <kimhyju703@naver.com>
2018-07-04 20:27:53 +0900

    친구삭제 기능 추가

--- app/src/main/java/com/gachon/kimhyju/tripool/activity/FriendlistActivity.java
+++ app/src/main/java/com/gachon/kimhyju/tripool/activity/FriendlistActivity.java
@@ -1,12 +1,17 @@
 package com.gachon.kimhyju.tripool.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.gachon.kimhyju.tripool.R;
@@ -21,7 +26,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class FriendlistActivity extends AppCompatActivity {
+public class FriendlistActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
     private NetworkService networkService;
     FriendAdapter friendAdapter;
     int user_id;
@@ -58,6 +63,7 @@ public class FriendlistActivity extends AppCompatActivity {
         Intent intent= getIntent();
         user_id=intent.getIntExtra("user_id",0);
         friendAdapter=new FriendAdapter(getApplicationContext());
+        listView.setOnItemLongClickListener(this);
     }
 
     public boolean onOptionsItemSelected(android.view.MenuItem item) {
@@ -67,6 +73,8 @@ public class FriendlistActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+
+
     };
 
 
@@ -98,6 +106,69 @@ public class FriendlistActivity extends AppCompatActivity {
             }
             @Override
             public void onFailure(Call<List<User>> user, Throwable t){
+                Log.d("MyTag(onFailure)","응답코드 : "+t.getMessage());
+            }
+        });
+    }
+
+
+
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id){
+        User user=(User)friendAdapter.getItem(position);
+        friend_id=user.getUser_id();
+        PopupMenu popup=new PopupMenu(FriendlistActivity.this,view);
+        getMenuInflater().inflate(R.menu.popupmenu,popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_deletefriend:
+                        showDialog();
+                        break;
+                }
+                return false;
+            }
+        });
+        popup.show();
+
+        return true;
+    }
+
+    public void showDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("친구 삭제하기");
+        builder.setMessage("정말로 친구를 삭제하시겠습니까?");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteTrip(user_id,friend_id);
+                getFriend(user_id);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+    }
+
+    public void deleteTrip(int user_id,int friend_id){
+        Call<User> deletefriend=networkService.delete_friend(user_id,friend_id);
+        deletefriend.enqueue(new Callback<User>(){
+            @Override
+            public void onResponse(Call<User> user, Response<User> response){
+                if(response.isSuccessful()){
+
+                }else{
+                    int statusCode=response.code();
+                    Log.d("MyTag(onResponse)","응답코드 : "+statusCode);
+                }
+            }
+            @Override
+            public void onFailure(Call<User> user, Throwable t){
                 Log.d("MyTag(onFailure)","응답코드 : "+t.getMessage());
             }
         });
